9b9f0b750b03ad9fe11985798e9d6472162355fd
AmanKumarVerma11 <akverma11aug2002@gmail.com>
2023-08-20 17:41:56 +0530

    Updates

--- src/tests/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "../components/App";
-
-test("renders the app", () => {
-  render(<App />);
-  const Element = screen.getByText(/portfolio/i);
-  expect(Element).toBeInTheDocument();
-});
